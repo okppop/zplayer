40774a23d2f30989f07323d8c5e9e2c649ad6583
okppop <okppop@protonmail.com>
2026-04-10 17:49:44 +0800

    feat: core/root

---
+++ src/core/root.zig
@@ -0,0 +1,7 @@
+const ch = @import("channel.zig");
+pub const Channel = ch.Channel;
+pub const ChannelError = ch.ChannelError;
+
+const rb = @import("ring_buffer.zig");
+pub const RingBuffer = rb.RingBuffer;
+pub const RingBufferError = rb.RingBufferError;
