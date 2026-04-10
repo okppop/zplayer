6f4e00df531a8373728ad4994fec987724eaaf80
okppop <okppop@protonmail.com>
2026-04-10 10:43:51 +0800

    fix: channel dead lock issue & close logic

--- src/core/channel.zig
+++ src/core/channel.zig
@@ -1,44 +1,66 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 const RingBuffer = @import("ring_buffer.zig").RingBuffer;
+const RingBufferError = @import("ring_buffer.zig").RingBufferError;
 
+pub const ChannelError = error{
+    ChannelClosed,
+};
+
+/// Channel is a concurrency safe queue implementation.
 pub fn Channel(comptime T: type) type {
     return struct {
         const Queue = RingBuffer(T);
         const Self = @This();
 
-        queue: Queue,
+        queue: *Queue,
         mu: std.Thread.Mutex = .{},
         not_empty: std.Thread.Condition = .{},
         not_full: std.Thread.Condition = .{},
+        is_closed: bool = false,
 
         pub fn init(allocator: Allocator, length: usize) !*Self {
             const self = try allocator.create(Self);
             self.* = .{
-                .rb = Queue.init(allocator, length),
+                .queue = try Queue.init(allocator, length),
             };
             return self;
         }
 
-        pub fn deinit(self: *Self, allocator: Allocator) !void {
-            try self.rb.deinit(allocator);
-            try allocator.destroy(self);
+        pub fn deinit(self: *Self, allocator: Allocator) void {
+            self.queue.deinit(allocator);
+            allocator.destroy(self);
         }
 
-        pub fn push(self: *Self, item: T) !void {
+        pub fn push(self: *Self, item: T) ChannelError!void {
             self.mu.lock();
             defer self.mu.unlock();
+            while (self.queue.is_full()) {
+                self.not_full.wait(&self.mu);
+                if (self.is_closed) return ChannelError.ChannelClosed;
+            }
 
-            while (self.queue.is_full()) self.not_full.wait(&self.mu);
-            return self.push(item);
+            self.queue.push(item) catch unreachable;
+            self.not_empty.signal();
         }
 
-        pub fn pop(self: *Self) !T {
+        pub fn pop(self: *Self) ChannelError!T {
             self.mu.lock();
             defer self.mu.unlock();
+            while (self.queue.is_empty()) {
+                self.not_empty.wait(&self.mu);
+                if (self.is_closed) return ChannelError.ChannelClosed;
+            }
 
-            while (self.queue.is_empty()) self.not_empty.wait(&self.mu);
-            return self.pop();
+            const item = self.queue.pop() catch unreachable;
+            self.not_full.signal();
+            return item;
+        }
+
+        pub fn close(self: *Self) void {
+            self.mu.lock();
+            defer self.mu.unlock();
+            self.is_closed = true;
         }
     };
 }
