579023f6cd8ef1424d8f4175bde139d81a2e42e3
okppop <okppop@protonmail.com>
2026-05-17 22:35:16 +0800

    feat: media/packet

---
+++ src/media/packet.zig
@@ -0,0 +1,17 @@
+const c = @import("c.zig").c;
+const internal = @import("internal.zig");
+
+const Self = @This();
+
+ptr: *c.AVPacket,
+
+pub fn init() !Self {
+    const ptr = c.av_packet_alloc() orelse return internal.ffmpeg.AVError;
+    return .{
+        .ptr = ptr.?,
+    };
+}
+
+pub fn deinit(self: Self) void {
+    c.av_packet_free(&self.ptr);
+}
